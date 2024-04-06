6b37576618f0fea7a992b48a640ee8d9d851d4df
Jakob Kellhuber <jakob.kellhuber@stud.th-rosenheim.de>
2024-04-06 22:22:20 +0200

    change DatabaseWrapper to use APIWrapper to update data

--- SQS_DEMO/app/wrapper/DatabaseWrapper.py
+++ SQS_DEMO/app/wrapper/DatabaseWrapper.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from APIWrapper import Weather
 
 Base = declarative_base()
 
@@ -19,40 +20,36 @@ class WeatherData(Base):
         return f"<WeatherData(city={self.city}, temperature={self.temperature}, last_update={self.last_update})>"
 
 
-class Database:
+class DatabaseWrapper:
     def __init__(self, db_path):
         self.engine = create_engine(f'sqlite:///{db_path}')
         Base.metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
+        self.api_wrapper = Weather()
 
-    def add_or_update_weather_data(self, city, temperature):
+    def get_temperature_by_city(self, city):
         """
-        Add new weather data to the database,
-        or update an existing entry if the last update was more than 15 minutes ago.
+        Get the most recent temperature for a specified city from the database, or from the OpenWeatherMap API if the data is outdated.
 
         :param city: The name of the city.
-        :param temperature: The temperature in the city.
+        :return: The most recent temperature for the city.
         """
         session = self.Session()
         data = session.query(WeatherData).filter_by(city=city).first()
-        if data is None:
-            # No existing data, add new row
-            new_data = WeatherData(city=city, temperature=temperature)
-            session.add(new_data)
-        elif data.last_update < datetime.utcnow() - timedelta(minutes=15):
-            # Existing data is more than 15 minutes old, update it
-            data.temperature = temperature
-        session.commit()
+        if data is None or data.last_update < datetime.utcnow() - timedelta(minutes=15):
+            # No data or data is outdated, get new temperature from API
+            current_weather = self.api_wrapper.get_weather_by_city(city)
+            temperature = current_weather['temp_c'] # todo verify
+            if data is None:
+                # No existing data, add new row
+                new_data = WeatherData(city=city, temperature=temperature)
+                session.add(new_data)
+            else:
+                # Existing data is outdated, update it
+                data.temperature = temperature
+            session.commit()
+        else:
+            # Data is up-to-date, use existing temperature
+            temperature = data.temperature
         session.close()
-
-    def get_weather_data(self, city):
-        """
-        Get the most recent weather data for a specified city.
-
-        :param city: The name of the city.
-        :return: A WeatherData object containing the most recent weather data for the city, or None if no data is found.
-        """
-        session = self.Session()
-        data = session.query(WeatherData).filter_by(city=city).order_by(WeatherData.last_update.desc()).first()
-        session.close()
-        return data
+        return temperature
