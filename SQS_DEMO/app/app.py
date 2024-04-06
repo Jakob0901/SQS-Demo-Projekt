7992851054987175c10882047afc4606877e7ac9
Jakob Kellhuber <jakob.kellhuber@stud.th-rosenheim.de>
2024-04-06 22:28:33 +0200

    added endpoint for temperature to application

--- SQS_DEMO/app/app.py
+++ SQS_DEMO/app/app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+from wrapper.DatabaseWrapper import DatabaseWrapper
 
-app = Flask(__name__)
 
+class TemperatureApp:
+    def __init__(self, db_path):
+        self.app = Flask(__name__)
+        self.db = DatabaseWrapper(db_path)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+        @self.app.route('/temperature/<city>')
+        def get_temperature(city):
+            temperature = self.db.get_temperature_by_city(city)
+            return jsonify({'city': city, 'temperature': temperature})
+
+    def run(self):
+        self.app.run()
 
 
 if __name__ == '__main__':
+    app = TemperatureApp('weather_data.db')
     app.run()
