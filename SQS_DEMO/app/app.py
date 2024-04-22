37fe8119f20d80d30826225c584c4d706f0ae044
Jakob Kellhuber <jakob.kellhuber@stud.th-rosenheim.de>
2024-04-22 08:59:23 +0200

    replace sqlite with postgressql

--- SQS_DEMO/app/app.py
+++ SQS_DEMO/app/app.py
@@ -1,30 +1,36 @@
 import os
 
-from wrapper.DatabaseWrapper import DatabaseWrapper
+from wrapper.DatabaseWrapper import DatabaseWrapper, DBConfig
 
 from flask import Flask, jsonify
 from flask_wtf.csrf import CSRFProtect
 
 
 class TemperatureApp:
-    def __init__(self, db_path):
+    def __init__(self, username, password):
         self.app = Flask(__name__)
-        self.app.config['SECRET_KEY'] = os.urandom(24)
+        self.app.config["SECRET_KEY"] = os.urandom(24)
 
         self.csrf = CSRFProtect()
         self.csrf.init_app(self.app)
 
-        self.db = DatabaseWrapper(db_path)
+        config = DBConfig(username=username,
+                          password=password,
+                          hostname="localhost",
+                          port=5432,
+                          database_name="SQS_Weather_DB")
 
-        @self.app.route('/temperature/<city>')
+        self.db = DatabaseWrapper(config=config)
+
+        @self.app.route("/temperature/<city>")
         def get_temperature(city):
             temperature = self.db.get_temperature_by_city(city)
-            return jsonify({'city': city, 'temperature': temperature})
+            return jsonify({"city": city, "temperature": temperature})
 
     def run(self):
         self.app.run()
 
 
-if __name__ == '__main__':
-    app = TemperatureApp('weather_data.db')
+if __name__ == "__main__":
+    app = TemperatureApp("SQS_DEMO", "kernschmelze")
     app.run()
--- SQS_DEMO/app/wrapper/DatabaseWrapper.py
+++ SQS_DEMO/app/wrapper/DatabaseWrapper.py
@@ -8,6 +8,21 @@ from APIWrapper import Weather
 Base = declarative_base()
 
 
+class DBConfig:
+    def __init__(self, username, password, hostname, port, database_name):
+        self.usr = username
+        self.pwd = password
+        self.hstnm = hostname
+        self.prt = port
+        self.db_nm = database_name
+
+    def get_connection_string(self):
+        return f"postgresql://{self.usr}:{self.pwd}@{self.hstnm}:{self.prt}/{self.db_nm}"
+
+    def __repr__(self):
+        return f"<DBConfig(username={self.usr}, hostname={self.hstnm}, port={self.prt}, database_name={self.db_nm})>"
+
+
 class WeatherData(Base):
     __tablename__ = 'weather_data'
 
@@ -21,8 +36,9 @@ class WeatherData(Base):
 
 
 class DatabaseWrapper:
-    def __init__(self, db_path):
-        self.engine = create_engine(f'sqlite:///{db_path}')
+
+    def __init__(self, config):
+        self.engine = create_engine(config.get_connection_string())
         Base.metadata.create_all(self.engine)
         self.session = sessionmaker(bind=self.engine)
         self.api_wrapper = Weather()
